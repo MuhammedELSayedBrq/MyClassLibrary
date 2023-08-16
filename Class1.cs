b4dd84611d5ae057355c86686ec7800c3d2845de
MuhammedELSayedBrq <mohamed999755@gmail.com>
2023-08-16 02:02:13 -0700

    init

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class operations
+    {
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int sum(int x, int y)
+        {
+            return x - y;
+        }
+
+    }
+}
